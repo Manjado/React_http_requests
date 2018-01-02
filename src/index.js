b6dfcf8373e70166063ec3b72cbea95c883d2e81
Manjado <michalalchimowicz@gmail.com>
2018-01-02 22:27:34 +0100

    interceptors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+	console.log(request);
+	return request;
+}, error => {
+	console.log(error);
+	//ten error działa np. jak nie ma neta
+	return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+	console.log(response);
+	return response;
+}, error => {
+	console.log(error);
+	//ten error działa np. jak nie ma neta
+	return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
